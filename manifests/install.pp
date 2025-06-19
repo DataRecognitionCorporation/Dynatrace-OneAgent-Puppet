72dc301793fe789954c96179815f06e905225a41
generees <grees@datarecognitioncorp.com>
2025-06-19 13:22:42 -0500

    install onlyif new version

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,6 +5,7 @@ class dynatraceoneagent::install {
 
   $created_dir              = $dynatraceoneagent::created_dir
   $download_dir             = $dynatraceoneagent::download_dir
+  $install_dir              = $dynatraceoneagent::install_dir
   $filename                 = $dynatraceoneagent::filename
   $download_path            = $dynatraceoneagent::download_path
   $provider                 = $dynatraceoneagent::provider
@@ -15,13 +16,32 @@ class dynatraceoneagent::install {
   $oneagent_puppet_conf_dir = $dynatraceoneagent::oneagent_puppet_conf_dir
 
   if ($::kernel == 'Linux' or $::osfamily  == 'AIX'){
+    $current_version_file = '/tmp/current_version.txt'
+    exec { 'get_latest_version':
+      command => "curl -s ${version_link} | jq -r .latestAgentVersion > /tmp/latest_version.txt",
+      path    => ['/usr/bin', '/bin'],
+    }
+    
+    exec {"touch_current_version":
+      command => "touch ${current_version_file}",
+      path    => ['/usr/bin', '/bin'],
+      unless  => "test -e ${install_dir}/agent/installer.version",
+    }
+
+    exec { 'get_current_version':
+      command => "cp ${install_dir}/agent/installer.version ${current_version_file}",
+      path    => ['/usr/bin', '/bin'],
+      onlyif  => "test -e ${install_dir}/agent/installer.version",
+    }
+
     exec { 'install_oneagent':
-        command   => $dynatraceoneagent::command,
-        cwd       => $download_dir,
-        timeout   => 6000,
-        creates   => $created_dir,
-        provider  => $provider,
-        logoutput => on_failure,
+      command   => $dynatraceoneagent::command,
+      cwd       => $download_dir,
+      timeout   => 6000,
+      creates   => $created_dir,
+      provider  => $provider,
+      logoutput => on_failure,
+      unless    => "diff -q ${current_version_file} /tmp/latest_version.txt",
     }
   }
 
